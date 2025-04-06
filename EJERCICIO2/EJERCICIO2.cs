5ce1993be4f38c9c88bf12ccca3d6659ab4317d4
jorge.camargo <jorge.camargo>
2025-04-05 23:19:56 -0500

    subiendo tarea 1

---
+++ EJERCICIO2/EJERCICIO2.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Entrada
+        Console.Write("Ingrese el primer valor: ");
+        int valor1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo valor: ");
+        int valor2 = int.Parse(Console.ReadLine());
+
+        // Proceso y salida
+        if (valor1 > valor2)
+        {
+            Console.WriteLine($"El mayor valor es: {valor1}");
+        }
+        else if (valor2 > valor1)
+        {
+            Console.WriteLine($"El mayor valor es: {valor2}");
+        }
+        else
+        {
+            Console.WriteLine("Los valores ingresados son iguales. Se esperaban valores distintos.");
+        }
+    }
+}
