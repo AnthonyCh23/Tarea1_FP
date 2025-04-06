6ca9289098808f30cf230adbbd309da65c89680b
jorge.camargo <jorge.camargo>
2025-04-05 23:41:07 -0500

    cambio ejercicio

--- EJERCICIO2/EJERCICIO2.cs
+++ EJERCICIO2/EJERCICIO2.cs
@@ -4,25 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Entrada
+        //Entrada
         Console.Write("Ingrese el primer valor: ");
         int valor1 = int.Parse(Console.ReadLine());
-
         Console.Write("Ingrese el segundo valor: ");
         int valor2 = int.Parse(Console.ReadLine());
 
-        // Proceso y salida
+        //Proceso
         if (valor1 > valor2)
         {
-            Console.WriteLine($"El mayor valor es: {valor1}");
+            Console.WriteLine("El mayor valor es: " + valor1);
         }
         else if (valor2 > valor1)
         {
-            Console.WriteLine($"El mayor valor es: {valor2}");
+            Console.WriteLine("El mayor valor es: " + valor2);
         }
         else
         {
-            Console.WriteLine("Los valores ingresados son iguales. Se esperaban valores distintos.");
+            Console.WriteLine("Los valores son iguales");
         }
     }
 }
